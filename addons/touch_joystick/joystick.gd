958ebed1c55108be3f53f9965f07af872de16493
Abdul Rauf <abdulroufsidhu@hotmail.com>
2025-01-26 23:53:33 +0500

    fixed joystick isolation issue

--- addons/touch_joystick/joystick.gd
+++ addons/touch_joystick/joystick.gd
@@ -13,6 +13,7 @@ signal direction
 		queue_redraw()
 		size = value
 
+var _shall_process: bool = false
 var knob_position: Vector2 = Vector2.ZERO
 
 func _input(event: InputEvent) -> void:
@@ -24,6 +25,8 @@ func _input(event: InputEvent) -> void:
 	
 	if (event is  InputEventScreenTouch):	
 		var touch: InputEventScreenTouch = event as InputEventScreenTouch
+		if (((touch.position - position)/size).length() < 1):
+			_shall_process = true
 		_process_input(event, touch.position)
 	
 	if (event is InputEventScreenDrag):
@@ -32,11 +35,14 @@ func _input(event: InputEvent) -> void:
 	#if (event is InputEventScreenTouch):
 	
 func _process_input(event: InputEvent, pos: Vector2) -> void:
+	if (not _shall_process): return
+	
 	var knob_pos = pos - position
 	if (event.is_released() and (event is InputEventScreenTouch and not event.is_pressed())):
 		knob_position = Vector2.ZERO
 		direction.emit(Vector2.ZERO)
 		queue_redraw()
+		_shall_process = false
 		return;
 	
 	var _direction_vector:Vector2 = (knob_pos/size).normalized()
